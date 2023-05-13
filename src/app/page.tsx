59ad0ea9c7dc96e8dc7b748855f23c24533e25e5
Tony Pujals <tpujals@gmail.com>
2023-05-12 23:02:15 -0700

    next app

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-2x1 font-bold text-center">Welcome to Learning Tailwind</h1>
+      <p className="mt-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </p>
+    </div>
+  );
+}
