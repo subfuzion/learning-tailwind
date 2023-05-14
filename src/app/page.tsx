f798dc255ec064c46e256be03b367e0072d387bb
Tony Pujals <tpujals@gmail.com>
2023-05-14 12:24:44 -0700

    updated global.css and tailwind.config.js styles, added links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,22 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="container mx-auto">
-      <h1 className="text-2x1 font-bold text-center">Welcome to Learning Tailwind</h1>
+      <h1 className="text-2x1 font-bold text-center">
+        Welcome to Learning Tailwind
+      </h1>
       <p className="mt-4">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
       </p>
+
+      <div>
+        <a href="./typography">Typography</a>
+      </div>
+      <div>
+        <a href="./vercel">Vercel</a>
+      </div>
+      <div>
+        <a href="./unvisited">Unvisited</a>
+      </div>
     </div>
   );
 }
---
+++ src/app/typography/page.tsx
@@ -0,0 +1,21 @@
+export default function page() {
+  return (
+    <>
+      <div className="container mx-auto">
+        <article className="prose">
+          <h1>Typography</h1>
+          <div>
+            <h2>Font</h2>
+            <h3>Family</h3>
+            <h3>Size</h3>
+            <h3>Weight</h3>
+            <h3>Style</h3>
+            <h2>Text</h2>
+            <h3>Color</h3>
+            <h3>Alignment</h3>
+          </div>
+        </article>
+      </div>
+    </>
+  );
+}
