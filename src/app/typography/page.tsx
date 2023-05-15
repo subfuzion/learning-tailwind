23530dbe1d61e97a62cf0dd0ccd328467c6c0171
Tony Pujals <tpujals@gmail.com>
2023-05-14 23:50:58 -0700

    experimenting with cards

--- src/app/typography/page.tsx
+++ src/app/typography/page.tsx
@@ -1,3 +1,37 @@
+interface CardProps {
+  title: string;
+  description?: any;
+  example?: string;
+  result?: any;
+  reference?: string;
+}
+
+function Card(props: CardProps) {
+  return (
+    <div className="max-w-full rounded overflow-hidden shadow-lg mt-4">
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{props.title}</div>
+        <div>{props.description}</div>
+
+        <div>
+          <div className="font-bold mt-8 mb-4 text-lg">Example</div>
+          <div className="font-mono">{props.example}</div>
+
+          <div className="font-bold mt-8 text-lg">Result</div>
+          <div className="font-serif text-lg">{props.result}</div>
+        </div>
+
+        {props.reference && (
+          <div>
+            <a href={props.reference} target="_blank">
+              Reference 🚀
+            </a>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
 export default function Typography() {
   return (
     <>
@@ -8,50 +42,65 @@ export default function Typography() {
           <div>
             <h2>Font</h2>
 
-            <div>
-              <h3>Family</h3>
-              <p>Use one of these three classes:</p>
-              <ul className="font-mono">
-                <li>font-sans</li>
-                <li>font-serif</li>
-                <li>font-mono</li>
-              </ul>
+            <Card
+                title="Font Family"
+                description={
+                  <>
+                    <p>Use one of these three classes:</p>
+                    <ul className="font-mono">
+                      <li>font-sans</li>
+                      <li>font-serif</li>
+                      <li>font-mono</li>
+                    </ul>
+                  </>
+                }
+                example='<p className="font-serif">Hello world</p>'
+                result={<p className="font-serif">Hello world</p>}
+                reference="https://tailwindcss.com/docs/font-family"
+            />
 
-              <div>
-                <h4>Example</h4>
-                <p className="font-mono">
-                  &lt;p className=&quot;font-serif&quot;&gt;Hello world&lt;/&gt;
-                </p>
-                <p className="font-serif">Hello world</p>
-                <a href="https://tailwindcss.com/docs/font-family">Reference</a>
-              </div>
-            </div>
+            <Card
+              title="Font Size"
+              description={
+                <>
+                  <p>Use one of these three classes:</p>
+                  <ul className="font-mono">
+                    <li>font-sans</li>
+                    <li>font-serif</li>
+                    <li>font-mono</li>
+                  </ul>
+                </>
+              }
+              example='<p className="font-serif">Hello world</p>'
+              result={<p className="font-serif">Hello world</p>}
+              reference="https://tailwindcss.com/docs/font-family"
+            />
 
-            <div>
-              <h3>Size</h3>
-              <p></p>
-            </div>
-            <div>
-              <h3>Weight</h3>
-              <p></p>
-            </div>
-            <div>
-              <h3>Style</h3>
-              <p></p>
-            </div>
+            {/*<div>*V/}
+            {/*  <h3>Size</h3>*/}
+            {/*  <p></p>*/}
+            {/*</div>*/}
+            {/*<div>*/}
+            {/*  <h3>Weight</h3>*/}
+            {/*  <p></p>*/}
+            {/*</div>*/}
+            {/*<div>*/}
+            {/*  <h3>Style</h3>*/}
+            {/*  <p></p>*/}
+            {/*</div>*/}
 
-            <div>
-              <h2>Text</h2>
+            {/*<div>*/}
+            {/*  <h2>Text</h2>*/}
 
-              <div>
-                <h3>Color</h3>
-                <p></p>
-              </div>
-              <div>
-                <h3>Alignment</h3>
-                <p></p>
-              </div>
-            </div>
+            {/*  <div>*/}
+            {/*    <h3>Color</h3>*/}
+            {/*    <p></p>*/}
+            {/*  </div>*/}
+            {/*  <div>*/}
+            {/*    <h3>Alignment</h3>*/}
+            {/*    <p></p>*/}
+            {/*  </div>*/}
+            {/*</div>*/}
           </div>
         </article>
       </div>
